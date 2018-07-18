90c452c4d67fc651f42ff391d3ac5488ec4d4dfd
qikaiqk <qikaiqk@gmail.com>
2018-07-18 16:35:43 +0800

    port modified

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.json(database.users) });
+// app.get('/', (req, res) => { res.json(database.users) });
+
+app.get('/', (req, res) => { res.send('it is working!') });
 
 app.post('/signin', (req, res) => { signin.signinHandler(req, res, db, bcrypt) });
 
@@ -36,6 +38,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 });
